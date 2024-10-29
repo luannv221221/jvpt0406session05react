459d959344b2a648e5f4796d18479e02a8985109
your new username <smallmonster900@gmail.com>
2024-10-29 20:35:52 +0700

    a

---
+++ src/data/product.js
@@ -0,0 +1,6 @@
+export const products = [
+    { id: 1, name: "sản phẩm 1", imag: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/08/Hinh-nen-Tet-14.jpg", price: 1000 },
+    { id: 2, name: "sản phẩm 2", imag: "https://tiki.vn/blog/wp-content/uploads/2023/12/anh-tet.jpeg", price: 1000 },
+    { id: 3, name: "sản phẩm 3", imag: "https://lavenderstudio.com.vn/wp-content/uploads/2023/12/concept-chup-hinh-tet-dep-nam-20243.jpg", price: 1000 },
+    { id: 4, name: "sản phẩm 4", imag: "https://bizweb.dktcdn.net/100/360/145/articles/chup-anh-tet-namnguyenstudio.jpg?v=1610013952857", price: 1000 },
+]
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { products } from '../data/product'
 
 const Product = () => {
     return (
-        <div>Product</div>
+        <>
+            <div
+                class="container"
+            >
+                <div
+                    class="row justify-content-center align-items-center g-2"
+                >
+                    {products.map((item, index) => {
+                        return (
+                            <div className='col-lg-3' key={index}>
+                                <div class="card">
+                                    <img class="card-img-top" style={{ maxHeight: '150px' }} src={item.imag} alt="Title" />
+                                    <div class="card-body">
+                                        <h4 class="card-title">{item.name}</h4>
+                                        <p class="card-text">{item.price}</p>
+                                    </div>
+                                    <a className='btn btn-secondary'>Mua ngay</a>
+                                </div>
+
+                            </div>
+                        )
+                    })}
+
+
+                </div>
+
+            </div>
+
+        </>
     )
 }
 
